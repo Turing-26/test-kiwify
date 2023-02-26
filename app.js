fe4b19a9e3ed0937e7160540c54ae35fb5844c67
Sarim26 <mail2sarim@gmail.com>
2023-02-25 20:14:29 -0800

    completed draft function

--- app.js
+++ app.js
@@ -6,17 +6,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/api/submit", (req, res) => {});
-
 app.post("/api/quiz", db.createQuiz);
 
+app.put("/api/quiz", db.editQuiz);
+
 app.post("/api/submit", db.giveQuiz);
-
-app.get("/api/users/:id", db.getUserById);
-
-app.get("/", db.getUsers);
-
-// app.post();
 
 app.listen(5000, () => {
   console.log("listening on port 5000");
--- db.js
+++ db.js
@@ -8,33 +8,13 @@ const pool = new Pool({
   port: 5432,
 });
 
-const getUsers = async (req, res) => {
-  try {
-    const users = await pool.query("select * from users");
-    res.status(200).json(users.rows);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const getUserById = async (request, response) => {
-  const id = request.params.id;
-
-  try {
-    const user = await pool.query(`select * from users where u_id = '${id}'`);
-    if (!user[0]) throw new Error("User not found");
-    response.status(200).json(user.rows);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 const createQuiz = async (request, response) => {
   try {
     const title = request.body.title;
     const description = request.body.description;
     const questions = request.body.questions;
 
+    // Here I used a dummy user_id because in the running application, the user id would be retrieved from the JWT token and not sent with the request, and since i did not work on the sign up logic for the test, i have just used a dummy user, also made in the sequel file for demonstration
     const quiz = await pool.query(
       `insert into quiz (u_id, title, description) values ('IUj7sDzCW7', '${title}', '${description}') returning q_id;`
     );
@@ -43,52 +23,98 @@ const createQuiz = async (request, response) => {
     questions.forEach(async (mcq) => {
       const { question, options, answer } = mcq;
       const optionValues = options.map((option) => `"${option}"`).join(", ");
-      const insertMcqQuery = `INSERT INTO questions (question, options, answer, q_id) VALUES ($1, '{${optionValues}}', $2, $3)`;
+      const insertMcqQuery = `insert into questions (question, options, answer, q_id) values ($1, '{${optionValues}}', $2, $3)`;
       await pool.query(insertMcqQuery, [question, answer, quizId]);
     });
 
-    response.status(200).send({ q_id: quizId, success: true });
+    response.status(200).send({ success: true, error: null, data: null });
+  } catch (error) {
+    response.status(404).send({
+      success: false,
+      errors: { code: error?.code, error: error },
+      data: null,
+    });
+  }
+};
+
+const editQuiz = async (request, response) => {
+  try {
+    const id = request.body?.id;
+    const title = request.body.title;
+    const description = request.body.description;
+    const questions = request.body.questions;
+
+    //Here once again, i assume a userId after jwt authentication
+
+    if (!id) {
+      const quiz = await pool.query(
+        `insert into quiz (u_id, title, description) values ('IUj7sDzCW7', '${title}', '${description}') returning q_id;`
+      );
+      const quizId = quiz.rows[0].q_id;
+
+      questions.forEach(async (mcq) => {
+        const { question, options, answer } = mcq;
+        const optionValues = options.map((option) => `"${option}"`).join(", ");
+        const insertMcqQuery = `insert into questions (question, options, answer, q_id) values ($1, '{${optionValues}}', $2, $3);`;
+        await pool.query(insertMcqQuery, [question, answer, quizId]);
+      });
+      response.status(200).send({ success: true, errors: null, data: null });
+      return;
+    }
+
+    await pool.query(
+      `update quiz set title = '${title}', description = '${description}' where q_id = '${id}';`
+    );
+    await pool.query(`delete from questions where q_id = '${id}';`);
+    questions.forEach(async (mcq) => {
+      const { question, options, answer } = mcq;
+      const optionValues = options.map((option) => `"${option}"`).join(", ");
+      const insertMcqQuery = `insert into questions (question, options, answer, q_id) values ($1, '{${optionValues}}', $2, $3);`;
+      await pool.query(insertMcqQuery, [question, answer, id]);
+    });
+
+    response.status(200).send({ success: true, error: null, data: null });
   } catch (error) {
     console.log(error);
   }
 };
 
-const giveQuiz = async (req, res) => {
-  const data = req.body;
-  const results = new Map();
-  const dbQuery = await pool.query(
-    `select * from questions where q_id = '${data.q_id}'`
-  );
-  console.log(
-    "--------------------------------------------------------------------------"
-  );
-  if (!dbQuery.rows.length) throw new Error("Quiz not found");
-  dbQuery.rows.forEach((question) => {
-    results.set(question.question_id, question.answer);
-  });
-  if (data.answers.length !== results.size)
-    throw new Error("Insufficient Answers");
+const giveQuiz = async (request, response) => {
+  try {
+    const data = request.body;
+    const results = new Map();
+    const dbQuery = await pool.query(
+      `select * from questions where q_id = '${data.q_id}'`
+    );
 
-  console.log(
-    "--------------------------------------------------------------------------"
-  );
-  let score = 0;
-  for (let i = 0; i < data.answers.length; i++) {
-    let res = results.get(data.answers[i].id);
-    if (res === data.answers[i].answer) score++;
+    if (!dbQuery.rows.length) throw new Error("Quiz not found");
+    dbQuery.rows.forEach((question) => {
+      results.set(question.question_id, question.answer);
+    });
+
+    if (data.answers.length !== results.size)
+      throw new Error("Insufficient Answers");
+
+    let score = 0;
+    for (let i = 0; i < data.answers.length; i++) {
+      let res = results.get(data.answers[i].id);
+      if (res === data.answers[i].answer) score++;
+    }
+
+    response.send({
+      success: true,
+      errors: null,
+      data: {
+        userScore: score,
+      },
+    });
+  } catch (error) {
+    response.send({ success: false, error: error, data: null });
   }
-  res.send({
-    success: true,
-    errors: null,
-    data: {
-      "User score": score,
-    },
-  });
 };
 
 module.exports = {
-  getUsers,
-  getUserById,
   createQuiz,
   giveQuiz,
+  editQuiz,
 };
